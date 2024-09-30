fb2dce04bfd2688c575748b982bb446457d71173
Brandon Cook <bcook2289@gmail.com>
2024-09-30 19:41:48 +0900

    feat: header design completed, need to work on animations

--- sentient-site/src/components/Header.tsx
+++ sentient-site/src/components/Header.tsx
@@ -7,46 +7,71 @@ import Youtube from "../public/icon/iconmonstr-youtube-9-240.png";
 import Spotify from "../public/icon/iconmonstr-spotify-4-240.png";
 import Apple from "../public/icon/iconmonstr-apple-os-4-240.png";
 
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+
 const Header = () => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["start start", "end start"],
+  });
+  const scaleLogo = useTransform(scrollYProgress, [0, 1], [1, 0.1]);
+  const moveLogoUp = useTransform(scrollYProgress, [0, 1], [0, -400]);
+
+  const moveNavDown = useTransform(scrollYProgress, [0, 1], [-2000, 0]);
+
   return (
     <>
       <header>
-        <div className="logo">
+        <motion.div
+          className="fixed top-0 left-0 w-full h-screen flex justify-center z-50"
+          style={{ scale: scaleLogo, y: moveLogoUp }}
+          ref={targetRef}
+        >
           <a href="#home">
-            <Image src={SentientLogo} width={100} height={100} alt="logo" />
+            <Image
+              src={SentientLogo}
+              fill
+              sizes="100vw"
+              style={{ objectFit: "contain" }}
+              alt="logo"
+            />
           </a>
-        </div>
-        <nav className="flex justify-center">
-          <div className="container mx-28 p-4">
-            <ul className="flex justify-between items-center">
-              <li>
-                <a href="#new-release" className="navlinks-link tidal">
-                  New Release
-                </a>
-              </li>
-              <li>
-                <a className="navlinks-link active" href="#about">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="#releases" className="navlinks-link boyw">
-                  Releases
-                </a>
-              </li>
-              <li>
-                <a href="#store" className="navlinks-link">
-                  Store
-                </a>
-              </li>
-              <li>
-                <a href="#contact" className="navlinks-link">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        </motion.div>
+        <motion.div style={{ y: moveNavDown }}>
+          <nav className="fixed bg-[#0a0a0a] w-full h-36 pt-24 flex justify-center align-end top-0 z-40">
+            <div className="container mx-auto flex justify-center p-4">
+              <ul className="flex justify-between items-center space-x-8">
+                <li>
+                  <a href="#new-release" className="navlinks-link tidal">
+                    New Release
+                  </a>
+                </li>
+                <li>
+                  <a className="navlinks-link active" href="#about">
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a href="#releases" className="navlinks-link boyw">
+                    Releases
+                  </a>
+                </li>
+                <li>
+                  <a href="#store" className="navlinks-link">
+                    Store
+                  </a>
+                </li>
+                <li>
+                  <a href="#contact" className="navlinks-link">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </motion.div>
       </header>
       <nav>
         <div className="socials-container">
