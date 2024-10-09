c38c51cf9046b8d67d09d4cbab0c870911f4b47e
Brandon Cook <bcook2289@gmail.com>
2024-10-09 16:37:39 +0900

    update: changed some styling for mobile friendly optimization and changed the store page to coming soon

--- sentient-site/src/components/About.tsx
+++ sentient-site/src/components/About.tsx
@@ -9,7 +9,7 @@ const About = () => {
     <>
       <div id="home"></div>
       <div
-        className="flex flex-col md:flex-row justify-center mx-4 md:m-32 my-10"
+        className="flex flex-col md:flex-row justify-center m-10 md:m-32 md:w-9/12"
         id="about"
       >
         <motion.div
@@ -21,24 +21,27 @@ const About = () => {
           <Image src={Light} sizes="500" alt="lightpath" />
         </motion.div>
         <motion.div
-          className="ml-10 w-1/2"
+          className="md:ml-10 md:w-1/2"
           initial={{ x: 100, opacity: 0 }}
           whileInView={{ x: 0, opacity: 1 }}
           transition={{ duration: 1, ease: "easeOut" }}
           viewport={{ once: true }}
         >
-          <h2 className="md:m-5 mt-5 mb-5 text-xl md:text-2xl font-bold uppercase tracking-wide">
-            We are Sentient
-          </h2>
-          <p className="w-screen">
-            A 5-piece band from Tokyo with modern metal, post-rock, and ambient
-            music influences If there is a singular concept behind the band, it
-            might be best described as an effort to transmute darkness and pain
-            into light, without constraints on style and execution. As the name
-            eludes to, the music of Sentient is about visceral feeling, and many
-            of the lyrical themes are derived from dark personal experiences and
-            finding the light and beauty in them.
-          </p>
+          <div className="w-full">
+            <h2 className="mt-5 md:mt-0 mb-5 text-xl md:text-2xl font-bold uppercase tracking-wide">
+              We are Sentient
+            </h2>
+            <p className="">
+              A 5-piece band from Tokyo with modern metal, post-rock, and
+              ambient music influences If there is a singular concept behind the
+              band, it might be best described as an effort to transmute
+              darkness and pain into light, without constraints on style and
+              execution. As the name eludes to, the music of Sentient is about
+              visceral feeling, and many of the lyrical themes are derived from
+              dark personal experiences and finding the light and beauty in
+              them.
+            </p>
+          </div>
         </motion.div>
       </div>
     </>
